81246a3438e8a6815d999b4653cff6ce64b5c34b
kwakseungmin23 <lookingforfandl@gmail.com>
2023-02-20 16:41:29 +0900

    login refactoring

--- routes/users.js
+++ routes/users.js
@@ -28,17 +28,15 @@ usersrouter.post("/signup", async (req, res) => {
     return res.status(500).send({ err: err.message });
   }
 });
-//login API
+//로그인
 usersrouter.post("/login", async (req, res) => {
   const { nickname, password } = req.body;
   const user = await User.findOne({ nickname });
 
-  //이메일 일치하는 유저가 존재하지 않거나, 유저를 찾았지만 비밀번호와 입력 비밀번호가 다를때,
   if (!user || user.password !== password) {
-    res.status(400).json({
-      err: "로그인 실패",
+    return res.status(400).json({
+      err: "wrong nickname or wrong password.",
     });
-    return;
   }
   const token = jwt.sign({ userId: user.userId }, "secret-key");
   res.cookie("Authorization", `Bearer ${token}`); // bearer type - jwt type
